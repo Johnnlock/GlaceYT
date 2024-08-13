523a7a5090ea9592588cc5397fc02e7c5a6bae81
zPolaco <31200227+Johnnlock@users.noreply.github.com>
2024-08-13 00:34:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Error 404 - Not found ❌"];
 
 
 let currentIndex = 0;
@@ -92,8 +92,8 @@ function updateStatusAndSendMessages() {
 
 client.once('ready', () => {
   console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨HAPPY NEW YEAR MY DEAR FAMILY`);
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️WELCOME TO 2024`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨Parabéns, Polaco Guardian online!`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️Aproveite e seja feliz!`);
   updateStatusAndSendMessages();
 
   setInterval(() => {
